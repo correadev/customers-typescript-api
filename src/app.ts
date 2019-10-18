52ca4a70f2b7afe790244dde3839450158b91f3f
Artur Correa <correa.dev@gmail.com>
2019-10-18 16:42:42 -0300

    Criado: configuração dotenv, mongo, bodyparser, express

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as mongoose from "mongoose";
+
+class App {
+
+    public app: express.Application = express();
+    public mongoUrl: string = 'mongodb://localhost:27017/CRMdb';
+
+    constructor() {
+        this.config();
+        this.mongoSetup();
+    }
+
+    private config(): void{
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(express.static('public'));
+    }
+
+    private mongoSetup(): void{
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl, {useNewUrlParser: true});        
+    }
+
+}
+
+export default new App().app;
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import app from './app';
+import { config } from 'dotenv';
+
+config();
+
+app.listen(process.env.API_PORT, () => {
+    console.log('Express server listening on port ' + process.env.API_PORT);
+})

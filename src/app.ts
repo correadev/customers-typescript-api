826098210b8e690b7e6efa9bb9681854372cca57
Artur Correa <correa.dev@gmail.com>
2019-10-18 17:04:30 -0300

    Criado: configuracoes nodemon e rota inicial

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,31 @@
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import * as mongoose from "mongoose";
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as mongoose from 'mongoose';
+import { Routes } from './customers/customerRoutes';
 
 class App {
 
-    public app: express.Application = express();
-    public mongoUrl: string = 'mongodb://localhost:27017/CRMdb';
+  public app: express.Application = express();
+  public routePrv: Routes = new Routes();
+  public mongoUrl: string = 'mongodb://localhost:27017/CRMdb';
 
-    constructor() {
-        this.config();
-        this.mongoSetup();
-    }
+  constructor() {
+    this.config();
+    this.mongoSetup();
+    this.routePrv.routes(this.app);     
+  }
 
-    private config(): void{
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({ extended: false }));
-        this.app.use(express.static('public'));
-    }
+  private config(): void{
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    // serving static files 
+    this.app.use(express.static('public'));
+  }
 
-    private mongoSetup(): void{
-        mongoose.Promise = global.Promise;
-        mongoose.connect(this.mongoUrl, {useNewUrlParser: true});        
-    }
+  private mongoSetup(): void{
+    mongoose.Promise = global.Promise;
+    mongoose.connect(this.mongoUrl, {useNewUrlParser: true});        
+  }
 
 }
 
--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,5 @@ import { config } from 'dotenv';
 config();
 
 app.listen(process.env.API_PORT, () => {
-    console.log('Express server listening on port ' + process.env.API_PORT);
+  console.log('Express server listening on port ' + process.env.API_PORT);
 })
